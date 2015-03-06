12471a50569d9b7accb72f9541bc0c09a5c33856
Elaine Yu <elaine@monsoonco.com>
2015-03-05 21:03:23 -0800

    save hw

--- readline.js
+++ readline.js
@@ -8,18 +8,79 @@ rl.prompt();
 
 // Version 1
 // Create a function that'll return your input
-rl.on('line', function(line) {
-  console.log("you said", line);
-  // provide input prompt again
-  rl.prompt();
-});
+// rl.on('line', function(line) {
+//   console.log("you said", line);
+//   // provide input prompt again
+//   rl.prompt();
+// });
 
 // Version 2
 // Create an array to save information
+// var toDoList = [];
+
+// rl.on('line', function(line) {
+//   toDoList.push(line);
+//   console.log(toDoList);
+//   rl.prompt();
+// });
+
+// Version 3
+// grab first word and set as command
+// rl.on('line', function(line) {
+//   var words = line.split(' '),
+//     command = words[0]
+//   console.log("the command is", command);
+//   console.log(words);
+//   rl.prompt();
+// });
+
+// Version 4
+// var toDoList = [];
+
+// rl.on('line', function(line) {
+//    var words = line.split(' '),
+//       command = words.shift();
+
+//    if(command === 'ls'){
+//       console.log(toDoList);
+//    }
+
+//    if(command === 'add') {
+//     var item = words.join(' ');
+//     toDoList.push(item);
+//    }
+//    rl.prompt();
+// });
+
+//  Invoke
+//  add foo
+//  add foo bar bar
+
+
+// Version 5
+// Create functions instead for add, remove, etc
+
+var commands = {
+  ls:function() {
+    console.log(toDoList);
+  },
+  add:function(item) {
+    toDoList.push(item);
+  }
+};
+
+
 var toDoList = [];
 
 rl.on('line', function(line) {
-  toDoList.push(line);
-  console.log(toDoList);
-  rl.prompt();
+   var words = line.split(' '),
+      command = words.shift();
+      argsStr = words.join();
+   commands[command](argsStr);
+   rl.prompt();
 });
+
+// Invoke
+// add apple
+// add pear
+// ls
